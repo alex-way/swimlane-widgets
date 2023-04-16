24040ad4fdec5a8e8a4d8da3dd7bd87d513c972a
Alex Way <alexwa@softcat.com>
2023-04-16 12:38:47 +0100

    Make import aliasing more generic

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,14 @@
 /** @type{import("@storybook/web-components-vite").StorybookConfig} */
 export default {
-	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx)"],
+	stories: ["../src/**/*.stories.@(js|jsx)"],
 	addons: [
 		"@storybook/addon-links",
 		"@storybook/addon-essentials",
 		"msw-storybook-addon",
-		"@storybook/addon-docs",
 	],
 	framework: {
 		name: "@storybook/web-components-vite",
 		options: {},
-	},
-	docs: {
-		autodocs: "tag",
 	},
 	core: {
 		builder: "@storybook/builder-vite",
--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,17 @@ export default defineConfig({
 		lib: {
 			formats: ["es"],
 		},
-		rollupOptions: {
-			external: /^lit/,
-		},
 	},
 	resolve: {
 		alias: {
+			"@swimlane/swimlane-element@1/templates.js":
+				"@swimlane/swimlane-element/lib/templates.js",
 			"@swimlane/swimlane-element@1": "@swimlane/swimlane-element",
-			"lit-html@1": "lit",
+			// For some reason, we need to alias the lit-html repeat directive to the lit repeat directive to
+			// get this working locally
+			// I think it's related to it importing typescript files, but I'm not sure
+			"lit-html@1/directives/": "lit/directives/",
+			"lit-html@1": "lit-html",
 			$components: `${__dirname}/src/components`,
 		},
 	},
