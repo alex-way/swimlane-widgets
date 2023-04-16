d566281a773a70e3f3d1ffc6c0def79f662178a6
Alex Way <alexwa@softcat.com>
2023-04-16 12:56:00 +0100

    Fix issue with importing

--- vite.config.js
+++ vite.config.js
@@ -9,14 +9,6 @@ export default defineConfig({
 	},
 	resolve: {
 		alias: {
-			"@swimlane/swimlane-element@1/templates.js":
-				"@swimlane/swimlane-element/lib/templates.js",
-			"@swimlane/swimlane-element@1": "@swimlane/swimlane-element",
-			// For some reason, we need to alias the lit-html repeat directive to the lit repeat directive to
-			// get this working locally
-			// I think it's related to it importing typescript files, but I'm not sure
-			"lit-html@1/directives/": "lit/directives/",
-			"lit-html@1": "lit-html",
 			$components: `${__dirname}/src/components`,
 		},
 	},
